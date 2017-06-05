9e818846331521e04b25978b45653b2b5d293db7
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-06-05 14:21:28 +0530

    Unit tests and integration tests were added.

---
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -0,0 +1,33 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from './CourseForm';
+
+function setUp(saving) {
+  const props = {
+    course: {}, saving: saving, errors: {},
+    onSave: () => {},
+    onChange: () => {}
+  };
+
+  return shallow(<CourseForm {...props} />);
+}
+
+describe('CourseForm via Enzyme', () => {
+  it('renders form and h1', () => {
+    const wrapper = setUp(false);
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h1').text()).toEqual('Manage Course');
+  });
+
+  it('save button is labeled "Save" when not saving', () => {
+    const wrapper = setUp(false);
+    expect(wrapper.find('input').props().value).toBe('Save');
+  })
+
+  it('save button is laveled "Saving" when saving', () => {
+    const wrapper = setUp(true);
+    expect(wrapper.find('input').props().value).toBe('Saving');
+  })
+})
